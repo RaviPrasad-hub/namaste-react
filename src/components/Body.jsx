27f44b7be4d141f93e496c857588167de75aed71
Ravi Prasad <51690127+RaviPrasad-hub@users.noreply.github.com>
2023-11-22 02:21:39 +0530

    one of the superpower of React

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData.js";
+import { useState } from "react";
 //not using keys is not acceptable <<<<<<< use index if unique is not present <<<<<<< unique id is highly recommended
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onMouseOver={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.card.card.info.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard
             key={restaurant.card.card.info.id}
             resData={restaurant}
